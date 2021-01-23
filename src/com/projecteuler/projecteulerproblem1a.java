38b1ae5a7fa7538c1019f7329de0cf092f346b26
lolercoaster <jpjinsu91@gmail.com>
2021-01-23 14:24:11 -0600

    more efficient of previous

--- src/com/projecteuler/projecteulerproblem1a.java
+++ src/com/projecteuler/projecteulerproblem1a.java
@@ -1,4 +1,20 @@
 package com.projecteuler;
 
+import java.util.Scanner;
+
 public class projecteulerproblem1a {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int sumj = 0;
+
+        for (int j = 1; j < x; ++j){
+            if (j%3 == 0 || j%5 ==0) { sumj += j;
+                System.out.println(j);
+            }
+        }
+        System.out.println(sumj);
+    }
 }
