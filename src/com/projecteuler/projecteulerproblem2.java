6d4dd89ca5d08af0f6b3e155f6356b5978e0fc30
lolercoaster <jpjinsu91@gmail.com>
2021-01-24 23:08:34 -0600

    projecteulerproblem2 solved

--- src/com/projecteuler/projecteulerproblem2.java
+++ src/com/projecteuler/projecteulerproblem2.java
@@ -1,2 +1,24 @@
-package com.projecteuler;public class projecteulerproblem2 {
+package com.projecteuler;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class projecteulerproblem2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Long> arrayList = new ArrayList<>();
+
+        long a = scanner.nextLong();
+        long sum = 0;
+        for (int i = 1; i <= a; i++) {
+            if (arrayList.size() > 2) {long x = arrayList.get(i-3); long y = arrayList.get(i-2);
+                arrayList.add(x+y);
+            }else {long z= i; arrayList.add(z);}}
+        System.out.println(arrayList);
+        for (long z : arrayList) {if (z%2 == 0) sum += z;}
+        System.out.println(sum);
+
+
+    }
 }
